10de3e7b9661b787ecd1b4f6a5d1306b6c12151a
Erick <erickmunoz13@gmail.com>
2024-08-22 22:14:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Azure Pipelines!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+module.exports = app;
+
